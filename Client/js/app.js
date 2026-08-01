03f701958ee6a63d31fe4369b951670cc3b7b8b9
HacenMario <stevenhacen@gmail.com>
2026-08-01 12:52:47 +0100

    Change API_BASE to an empty string

--- Client/js/app.js
+++ Client/js/app.js
@@ -1,5 +1,5 @@
 // ===================== إعدادات =====================
-const API_BASE = 'https://khireksab9ek.onrender.com/api';
+const API_BASE = '';
 const VAPID_PUBLIC_KEY = 'BF7IlardTlVn6X4dNtcTad2ixM09jH87Q-vKyo5ScWY9uzLw3y-goXcgPmC8gxBpFWIGVgFWKxwC2pTDXNYnlD4';
 
 let map;
